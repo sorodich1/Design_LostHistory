e7e8611dd28c1f98e3177a14f5ae7a55a2f95efd
sorodich1 <sorodich@gmail.com>
2022-02-28 22:31:41 +0300

    Рефакторинг

--- js/main.js
+++ js/main.js
@@ -11,3 +11,31 @@
         }
     };
 }());
+function openNav()
+{
+    document.getElementById("sidebar").style.width = "323px";
+    //document.getElementById("main").style.marginLeft = "323px";
+    //document.getElementById("header").style.marginLeft = "323px";
+}
+function closeNav()
+{
+    document.getElementById("sidebar").style.width = "0";
+    //document.getElementById("main").style.marginLeft = "0";
+   // document.getElementById("header").style.marginLeft = "0";
+}
+function chAngeImage(src)
+{
+    document.getElementById('icon').src = src;
+}
+function chAngeImage2(src)
+{
+    document.getElementById('icon2').src = src;
+}
+function chAngeImage3(src)
+{
+    document.getElementById('icon3').src = src;
+}
+function chAngeImage4(src)
+{
+    document.getElementById('icon4').src = src;
+}
